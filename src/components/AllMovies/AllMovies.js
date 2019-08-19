e04f1defa24d588280106a190223ce0feb7becf7
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-08-19 15:47:45 -0600

    api mapping working on the front end!

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -3,27 +3,36 @@ import MovieCard from '../MovieCard/MovieCard'
 import Header from '../Header/Header'
 import './AllMovies.css'
 import Footer from '../Footer/Footer'
+import axios from 'axios'
 
 export default class AllMovies extends Component {
+    state = {
+        movieData: []
+    }
+    componentDidMount() {
+        axios.get(`api/movies`)
+            .then(res => {
+                this.setState({ movieData: res.data })
+            })
+    }
     render() {
+        const mappedMovies = this.state.movieData.map(element => {
+            return <MovieCard
+                poster={element.poster}
+            />
+        })
         return (
             <div className='all-movies'>
                 <Header />
                 <hr class='hr'></hr>
                 <div className='movie-container'>
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
+                    {mappedMovies}
+
                 </div>
                 <hr class='hr'></hr>
-                    <div>
-                        <Footer />
-                    </div>
+                <div>
+                    <Footer />
+                </div>
             </div>
         )
     }
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,26 +5,11 @@ import './MovieCard.css'
 
 
 export default class MovieCard extends Component {
-    state = {
-        movieData: []
-    }
-    componentDidMount() {
-        axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&i=tt1276104`)
-            .then(res => {
-                this.setState({ movieData: [res.data] })
-            })
-    }
     render() {
-        const mappedMovies = this.state.movieData.map(element => {
-            return <div id='movie-img'>
-                <img src={element.Poster} alt="" />
-            </div>
-        })
         return (
             <div className='movie-card grow'>
                 <Rental />
-                {mappedMovies}
-
+                <img src={this.props.poster} alt="poster" />
             </div>
         )
     }
